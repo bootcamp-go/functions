d48cecfa72b5d67ab9b2dec7febc897b629addaf
jean <ext_jecardo@mercadolibre.com>
2023-08-25 15:00:43 -0300

    [added] functions excercise for practising

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/m/funciones
+
+go 1.20
---
+++ sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// FUNCIONES
+
+// Parametros (opcionales)
+
+// Nombre (obligatorio)
+
+// Retornos (Opcionales)
+
+func main() {
+	result := sum(7, 5)
+	fmt.Println("The result of the operation was: ", result)
+}
+
+func sum(num1, num2 int) int {
+	return num1 + num2
+}
